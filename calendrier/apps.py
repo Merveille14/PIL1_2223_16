e3d0ecdd0aa8871ba9543a373d728da1a4084189
Elzab777 <elzabadboussari@gmail.com>
2023-06-23 14:45:51 +0200

    etudiant commit

--- calendrier/apps.py
+++ calendrier/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class EmploieTempsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'emploie_temps'
+    name = 'calendrier'
